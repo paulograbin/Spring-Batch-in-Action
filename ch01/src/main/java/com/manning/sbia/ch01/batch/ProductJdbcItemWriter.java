0b464f6e3f1aa48d05d5f0bef63c56f63f961f3d
Paulo Grabin <paulograbin@gmail.com>
2025-06-22 22:07:52 -0300

    Reformat code

--- ch01/src/main/java/com/manning/sbia/ch01/batch/ProductJdbcItemWriter.java
+++ ch01/src/main/java/com/manning/sbia/ch01/batch/ProductJdbcItemWriter.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.manning.sbia.ch01.batch;
 
 import java.util.List;
@@ -12,39 +9,29 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import com.manning.sbia.ch01.domain.Product;
 
-/**
- * @author acogoluegnes
- *
- */
+
 public class ProductJdbcItemWriter implements ItemWriter<Product> {
-	
-	private static final String INSERT_PRODUCT = "insert into product (id,name,description,price) values(?,?,?,?)";
-	
-	private static final String UPDATE_PRODUCT = "update product set name=?, description=?, price=? where id = ?";
-	
-	private JdbcTemplate jdbcTemplate;
-	
-	public ProductJdbcItemWriter(DataSource dataSource) {
-		this.jdbcTemplate = new JdbcTemplate(dataSource);
-	}
 
-	/* (non-Javadoc)
-	 * @see org.springframework.batch.item.ItemWriter#write(java.util.List)
-	 */
-	public void write(List<? extends Product> items) throws Exception {
-		for(Product item : items) {
-			System.out.println("opa opa");
+    private static final String INSERT_PRODUCT = "insert into product (id,name,description,price) values(?,?,?,?)";
+    private static final String UPDATE_PRODUCT = "update product set name=?, description=?, price=? where id = ?";
 
-			int updated = jdbcTemplate.update(UPDATE_PRODUCT,
-				item.getName(),item.getDescription(),item.getPrice(),item.getId()
-			);
-			if(updated == 0) {
-				jdbcTemplate.update(
-					INSERT_PRODUCT,
-					item.getId(),item.getName(),item.getDescription(),item.getPrice()
-				);	
-			}								
-		}
-	}
+    private JdbcTemplate jdbcTemplate;
 
+    public ProductJdbcItemWriter(DataSource dataSource) {
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+    public void write(List<? extends Product> items) throws Exception {
+        for (Product item : items) {
+            int updated = jdbcTemplate.update(UPDATE_PRODUCT,
+                    item.getName(), item.getDescription(), item.getPrice(), item.getId()
+            );
+            if (updated == 0) {
+                jdbcTemplate.update(
+                        INSERT_PRODUCT,
+                        item.getId(), item.getName(), item.getDescription(), item.getPrice()
+                );
+            }
+        }
+    }
 }
