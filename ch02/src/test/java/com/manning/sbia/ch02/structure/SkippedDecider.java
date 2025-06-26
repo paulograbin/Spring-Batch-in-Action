e822c90c8692f747a7dcf3dd2f05d2223657d42b
Paulo Grabin <paulograbin@gmail.com>
2025-06-26 15:00:48 -0300

    Reformat

--- ch02/src/test/java/com/manning/sbia/ch02/structure/SkippedDecider.java
+++ ch02/src/test/java/com/manning/sbia/ch02/structure/SkippedDecider.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.manning.sbia.ch02.structure;
 
 import org.springframework.batch.core.JobExecution;
@@ -8,19 +5,11 @@ import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.job.flow.FlowExecutionStatus;
 import org.springframework.batch.core.job.flow.JobExecutionDecider;
 
-/**
- * @author acogoluegnes
- *
- */
 public class SkippedDecider implements JobExecutionDecider {
 
-	/* (non-Javadoc)
-	 * @see org.springframework.batch.core.job.flow.JobExecutionDecider#decide(org.springframework.batch.core.JobExecution, org.springframework.batch.core.StepExecution)
-	 */
-	@Override
-	public FlowExecutionStatus decide(JobExecution jobExecution,
-			StepExecution stepExecution) {
-		return stepExecution.getSkipCount() == 0 ? FlowExecutionStatus.COMPLETED : new FlowExecutionStatus("SKIPPED");
-	}
+    @Override
+    public FlowExecutionStatus decide(JobExecution jobExecution, StepExecution stepExecution) {
+        return stepExecution.getSkipCount() == 0 ? FlowExecutionStatus.COMPLETED : new FlowExecutionStatus("SKIPPED");
+    }
 
 }
