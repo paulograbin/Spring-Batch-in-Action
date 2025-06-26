adf0fadbf8204a32c2b7a9ef9792dd024e1335c1
Paulo Grabin <plgrabin@gmail.com>
2025-06-25 21:58:18 -0300

    Fix input file location and make it run in mysql

--- ch02/src/main/java/com/manning/sbia/ch02/LaunchImportProductsJob.java
+++ ch02/src/main/java/com/manning/sbia/ch02/LaunchImportProductsJob.java
@@ -1,5 +1,5 @@
 /**
- * 
+ *
  */
 package com.manning.sbia.ch02;
 
@@ -16,26 +16,26 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  */
 public class LaunchImportProductsJob {
 
-	/**
+    /**
      * @param args
      */
     public static void main(String[] args) throws Exception {
-            ApplicationContext ctx = new ClassPathXmlApplicationContext(
-                    "/import-products-job-context.xml",
-                    "/com/manning/sbia/ch02/batch-infrastructure-context.xml",
-        			"/com/manning/sbia/ch02/connect-database-context.xml"
-            );
-            
-            JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
-            Job job = ctx.getBean(Job.class);       
-            
-            jobLauncher.run(job, new JobParametersBuilder()
-                    .addString("inputResource", "file:./products.zip")
-                    .addString("targetDirectory", "./importproductsbatch/")
-                    .addString("targetFile","products.txt")
-                    .addString("date", "2010-06-27")
-                    .toJobParameters()
-            );
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(
+                "/import-products-job-context.xml",
+                "/com/manning/sbia/ch02/batch-infrastructure-context.xml",
+                "/com/manning/sbia/ch02/connect-database-context.xml"
+        );
+
+        JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
+        Job job = ctx.getBean(Job.class);
+
+        jobLauncher.run(job, new JobParametersBuilder()
+                .addString("inputResource", "classpath:/input/products.zip")
+                .addString("targetDirectory", "./importproductsbatch/")
+                .addString("targetFile", "products.txt")
+                .addString("date", "2010-06-27")
+                .toJobParameters()
+        );
     }
-	
+
 }
