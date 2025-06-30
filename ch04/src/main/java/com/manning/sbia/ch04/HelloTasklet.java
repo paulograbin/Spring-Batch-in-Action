463addefe47ef2e7a210dc09f784f1ad8a63ab67
Paulo Grabin <paulograbin@gmail.com>
2025-06-30 10:16:06 -0300

    Just reformatting

--- ch04/src/main/java/com/manning/sbia/ch04/HelloTasklet.java
+++ ch04/src/main/java/com/manning/sbia/ch04/HelloTasklet.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.manning.sbia.ch04;
 
 import java.util.Map;
@@ -12,31 +9,23 @@ import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 
-/**
- * @author acogoluegnes
- *
- */
 public class HelloTasklet implements Tasklet {
 
-	/* (non-Javadoc)
-	 * @see org.springframework.batch.core.step.tasklet.Tasklet#execute(org.springframework.batch.core.StepContribution, org.springframework.batch.core.scope.context.ChunkContext)
-	 */
-	@Override
-	public RepeatStatus execute(StepContribution contribution,
-			ChunkContext chunkContext) throws Exception {
-		JobParameters jobParameters = chunkContext.getStepContext().getStepExecution().getJobParameters();
-		if(jobParameters.isEmpty()) {
-			System.out.println("No job parameters!");
-		} else {
-			System.out.println("Job parameters:");
-			for(Map.Entry<String, JobParameter> param : jobParameters.getParameters().entrySet()) {
-				System.out.println(String.format(
-					"%s = %s (%s)",
-					param.getKey(),param.getValue().getValue(),param.getValue().getType()
-				));
-			}
-		}
-		return RepeatStatus.FINISHED;
-	}
+    /* (non-Javadoc)
+     * @see org.springframework.batch.core.step.tasklet.Tasklet#execute(org.springframework.batch.core.StepContribution, org.springframework.batch.core.scope.context.ChunkContext)
+     */
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+        JobParameters jobParameters = chunkContext.getStepContext().getStepExecution().getJobParameters();
+        if (jobParameters.isEmpty()) {
+            System.out.println("No job parameters!");
+        } else {
+            System.out.println("Job parameters:");
+            for (Map.Entry<String, JobParameter> param : jobParameters.getParameters().entrySet()) {
+                System.out.println(String.format("%s = %s (%s)", param.getKey(), param.getValue().getValue(), param.getValue().getType()));
+            }
+        }
+        return RepeatStatus.FINISHED;
+    }
 
 }
--- ch04/src/main/java/com/manning/sbia/ch04/ModifyExitStatusListener.java
+++ ch04/src/main/java/com/manning/sbia/ch04/ModifyExitStatusListener.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.manning.sbia.ch04;
 
 import org.springframework.batch.core.ExitStatus;
@@ -8,20 +5,17 @@ import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.StepExecution;
 import org.springframework.batch.core.listener.StepExecutionListenerSupport;
 
-/**
- * @author acogoluegnes
- *
- */
 public class ModifyExitStatusListener extends StepExecutionListenerSupport {
 
-	@Override
-	public ExitStatus afterStep(StepExecution stepExecution) {
-		JobParameters jobParameters = stepExecution.getJobExecution().getJobInstance().getJobParameters();
-		String exitStatus = jobParameters.getString("exit.status");
-		if(exitStatus != null) {
-			return new ExitStatus(exitStatus);
-		}
-		return super.afterStep(stepExecution);
-	}
-	
+    @Override
+    public ExitStatus afterStep(StepExecution stepExecution) {
+        JobParameters jobParameters = stepExecution.getJobExecution().getJobInstance().getJobParameters();
+        String exitStatus = jobParameters.getString("exit.status");
+
+        if (exitStatus != null) {
+            return new ExitStatus(exitStatus);
+        }
+        return super.afterStep(stepExecution);
+    }
+
 }
