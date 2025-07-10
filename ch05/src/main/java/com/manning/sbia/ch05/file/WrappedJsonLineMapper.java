c9e799e5a5e80e635a47c1b211c76b3672e2a8d3
Paulo Grabin <plgrabin@gmail.com>
2025-07-10 17:55:33 -0300

    Adjust spacing

--- ch05/src/main/java/com/manning/sbia/ch05/file/WrappedJsonLineMapper.java
+++ ch05/src/main/java/com/manning/sbia/ch05/file/WrappedJsonLineMapper.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package com.manning.sbia.ch05.file;
 
 import java.util.Map;
@@ -10,28 +7,24 @@ import org.springframework.batch.item.file.mapping.JsonLineMapper;
 
 import com.manning.sbia.ch05.Product;
 
-/**
- * @author templth
- *
- */
 public class WrappedJsonLineMapper implements LineMapper<Product> {
-	private JsonLineMapper delegate;
 
-	public Product mapLine(String line, int lineNumber) throws Exception {
-  		  Map<String,Object> productAsMap
-  		                 = delegate.mapLine(line, lineNumber);
+    private JsonLineMapper delegate;
 
-		Product product = new Product();
-		product.setId((String)productAsMap.get("id"));
-		product.setName((String)productAsMap.get("name"));
-		product.setDescription((String)productAsMap.get("description"));
-		product.setPrice(new Float((Double)productAsMap.get("price")));
+    public Product mapLine(String line, int lineNumber) throws Exception {
+        Map<String, Object> productAsMap = delegate.mapLine(line, lineNumber);
 
-		return product;
-	}
+        Product product = new Product();
+        product.setId((String) productAsMap.get("id"));
+        product.setName((String) productAsMap.get("name"));
+        product.setDescription((String) productAsMap.get("description"));
+        product.setPrice(new Float((Double) productAsMap.get("price")));
 
-	public void setDelegate(JsonLineMapper delegate) {
-		this.delegate = delegate;
-	}
+        return product;
+    }
+
+    public void setDelegate(JsonLineMapper delegate) {
+        this.delegate = delegate;
+    }
 }
 
--- ch05/src/test/java/com/manning/sbia/ch05/database/JobStructureHibernateCursorTest.java
+++ ch05/src/test/java/com/manning/sbia/ch05/database/JobStructureHibernateCursorTest.java
@@ -6,18 +6,12 @@ import org.springframework.test.context.ContextConfiguration;
 
 import com.manning.sbia.ch05.AbstractJobStructureTest;
 
-import java.util.List;
-
 @ContextConfiguration
 public class JobStructureHibernateCursorTest extends AbstractJobStructureTest {
 
     @Test
     public void hibernateCursorJob() throws Exception {
         jobLauncher.run(job, new JobParameters());
-
-        List<String> products = List.of("PR....210", "PR....211", "PR....212", "PR....213", "PR....214", "PR....215", "PR....216", "PR....217");
-
-        checkProducts(writer.getProducts(), products.toArray(String[]::new));
+        checkProducts(writer.getProducts(), new String[]{"PR....210", "PR....211", "PR....212", "PR....213", "PR....214", "PR....215", "PR....216", "PR....217"});
     }
-
 }
