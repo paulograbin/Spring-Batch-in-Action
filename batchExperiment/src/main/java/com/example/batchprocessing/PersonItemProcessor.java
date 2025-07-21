52968fb29e4cba1de7c57acff0e2c11e9c6dc2a7
Paulo Grabin <plgrabin@gmail.com>
2025-07-21 17:53:54 -0300

    Work with class, not record

--- batchExperiment/src/main/java/com/example/batchprocessing/PersonItemProcessor.java
+++ batchExperiment/src/main/java/com/example/batchprocessing/PersonItemProcessor.java
@@ -11,10 +11,12 @@ public class PersonItemProcessor implements ItemProcessor<Person, Person> {
 
 	@Override
 	public Person process(final Person person) {
-		final String firstName = person.firstName().toUpperCase();
-		final String lastName = person.lastName().toUpperCase();
+		final String firstName = person.firstName.toUpperCase();
+		final String lastName = person.lastName.toUpperCase();
 
-		final Person transformedPerson = new Person(firstName, lastName);
+		final Person transformedPerson = new Person();
+		transformedPerson.firstName = firstName;
+		transformedPerson.lastName = lastName;
 
 		log.info("Converting ({}) into ({})", person, transformedPerson);
 
