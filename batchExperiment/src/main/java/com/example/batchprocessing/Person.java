13d98d283141a81d8e4805cc52f2ac52e26764c3
Paulo Grabin <plgrabin@gmail.com>
2025-07-21 17:52:43 -0300

    convert person to regular class

--- batchExperiment/src/main/java/com/example/batchprocessing/Person.java
+++ batchExperiment/src/main/java/com/example/batchprocessing/Person.java
@@ -1,5 +1,28 @@
 package com.example.batchprocessing;
 
-public record Person(String firstName, String lastName) {
+public class Person {
 
+    public String firstName;
+    public String lastName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + firstName + " " + lastName + '}';
+    }
 }
