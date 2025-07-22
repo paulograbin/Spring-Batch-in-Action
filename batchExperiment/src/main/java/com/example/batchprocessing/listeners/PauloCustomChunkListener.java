d9fe366f27d5c9decf33ac7ed202d87fc3859415
Paulo Grabin <paulograbin@gmail.com>
2025-07-21 22:52:45 -0300

    Modify chunk listener

--- batchExperiment/src/main/java/com/example/batchprocessing/listeners/PauloCustomChunkListener.java
+++ batchExperiment/src/main/java/com/example/batchprocessing/listeners/PauloCustomChunkListener.java
@@ -5,17 +5,28 @@ import org.slf4j.LoggerFactory;
 import org.springframework.batch.core.ChunkListener;
 import org.springframework.batch.core.scope.context.ChunkContext;
 
+import java.util.Map;
+
 public class PauloCustomChunkListener implements ChunkListener {
 
     private static final Logger LOG = LoggerFactory.getLogger(PauloCustomChunkListener.class);
 
     @Override
     public void beforeChunk(ChunkContext context) {
-//        LOG.info("before chunk");
+        LOG.info("***** New chunk ******");
+    }
+
+    @Override
+    public void afterChunkError(ChunkContext context) {
+        LOG.warn("Chunk error!!!!!!!!!!!!!!!!!");
     }
 
     @Override
     public void afterChunk(ChunkContext context) {
-//        LOG.info("after chunk");
+        Map<String, Object> stepExecutionContext = context.getStepContext().getStepExecutionContext();
+        String orDefault = String.valueOf(stepExecutionContext.get("personItemReader.read.count"));
+        String rollbackCount = String.valueOf(stepExecutionContext.get("rollbackCount"));
+
+        LOG.info("***** Chunk ended after reading {} items and {} rollbacks ******", orDefault, rollbackCount);
     }
 }
